300a7d554806f92bbf1c2950449d6fd7d5c73c69
Artessay <rihong.edu@outlook.com>
2024-07-09 11:15:08 +0800

    refactor insert

--- schema_linking/SchemaLinker.py
+++ schema_linking/SchemaLinker.py
@@ -1,3 +1,6 @@
+import sqlite3
+from typing import List
+
 """
 This is the interface class for schema linking.
 """
@@ -12,3 +15,36 @@ class SchemaLinker():
             query: str, 
     ) -> str:
         raise NotImplementedError
+
+    def _construct_insert_statements(self, cursor: sqlite3.Cursor, table_name: str, column_names: List[str]) -> str:
+        """
+        Construct insert statements for each row in the table.
+        """
+        insert_statements = ""
+        column_names_str = ", ".join([f"`{column_name}`" for column_name in column_names])
+
+        # Get the first few rows of the current table for example insert statements
+        cursor.execute(f"SELECT {column_names_str} FROM `{table_name}` LIMIT {self.num_insert_rows};")
+        rows = cursor.fetchall()
+
+        if rows:
+            # Create INSERT statements for the sample data
+            for row in rows:
+                # Properly format the INSERT statement with the actual row values
+                row_values = ', '.join([self._format_sql_value(value) for value in row])
+                insert_statements += f"INSERT INTO `{table_name}` VALUES ({row_values});\n"
+                # insert_statements += f"INSERT INTO `{table_name}` ({column_names_str}) VALUES ({row_values});\n"
+
+            # Add extra newline for formatting if there were any INSERT statements
+            insert_statements += "\n"
+
+        return insert_statements
+
+    def _format_sql_value(self, value) -> str:
+        if value is None:
+            return "NULL"
+        elif isinstance(value, str):
+            value = value.replace("'", "''")
+            return f"'{value}'"
+        else:
+            return str(value)
--- schema_linking/VanillaLinker.py
+++ schema_linking/VanillaLinker.py
@@ -24,34 +24,14 @@ class VanillaLinker(SchemaLinker):
         for table_name, create_statement in (result for result in results if result[1] is not None):
             schema += create_statement + ";\n\n"
 
-            # Get the first few rows of the current table for example insert statements
-            cursor.execute(f"SELECT * FROM `{table_name}` LIMIT {self.num_insert_rows};")
-            rows = cursor.fetchall()
-            if rows:
-                # Get column names for the current table
-                cursor.execute(f"PRAGMA table_info(`{table_name}`);")
-                columns_info = cursor.fetchall()
-                column_names = ", ".join([f"`{info[1]}`" for info in columns_info])
+            # Get column names for the current table
+            cursor.execute(f"PRAGMA table_info(`{table_name}`);")
+            columns_info = cursor.fetchall()
+            column_names = [info[1] for info in columns_info]
 
-                # Create INSERT statements for the sample data
-                for row in rows:
-                    # Properly format the INSERT statement with the actual row values
-                    row_values = ', '.join([self._format_value(value) for value in row])
-                    insert_statement = f"INSERT INTO `{table_name}` VALUES ({row_values});"
-                    # insert_statement = f"INSERT INTO `{table_name}` ({column_names}) VALUES ({row_values});"
-                    schema += insert_statement + "\n"
-
-                # Add extra newline for formatting if there were any INSERT statements
-                schema += "\n"
+            insert_statements = self._construct_insert_statements(cursor, table_name, column_names)
+            schema += insert_statements
 
         conn.close()
         return schema
     
-    def _format_value(self, value) -> str:
-        if value is None:
-            return "NULL"
-        elif isinstance(value, str):
-            value = value.replace("'", "''")
-            return f"'{value}'"
-        else:
-            return str(value)
